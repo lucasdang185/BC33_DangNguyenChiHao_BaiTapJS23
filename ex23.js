6fb2a900a86b593cfa4cb138c141feeee65b4218
lucasdang <dangnguyenchihao1@gmail.com>
2022-07-22 16:57:16 +0700

    done

--- ex23.js
+++ ex23.js
@@ -42,3 +42,70 @@ function ex2(){
   var rs = (num1+num2+num3+num4+num5)/5;
   document.getElementById("rs2").innerHTML= rs;
 }
+
+/**Bài 3: Quy đổi tiền tệ
+ * 
+ * input: nhập số tiền usd
+ * process: 
+ * 1: lấy input 
+ * 2: giá trị tiền vn = tiền usd * 23500;
+ * 3: in kết quả ra màn hình
+ * 
+ * output: số tiền vn sau khi quy đổi
+*/
+
+function ex3(){
+  var num = parseInt(document.getElementById("txtNumberUSD").value);
+  if(num < 0 ){
+    return document.getElementById("rs3").innerHTML = "Kiểm tra số USD vừa nhập!"
+  }
+  var rs = num * 23500;
+  document.getElementById("rs3").innerHTML= rs;
+}
+
+/**Bài 4: Tính diện tích, chu vi hình chữ nhật
+ * 
+ * input: nhập chiều dài + rộng
+ * process: 
+ * 1: lấy input 
+ * 2: chu vi = (dài + rộng) * 2
+ * 3: diện tích = dài * rộng
+ * 4: in kết quả ra màn hình
+ * 
+ * output: diện tích, chu vi hình chữ nhật
+*/
+
+function ex4(){
+  var num1 = parseInt(document.getElementById("txtDai").value);
+  var num2 = parseInt(document.getElementById("txtRong").value);
+
+  if(num1 < 0 || num2 < 0){
+    return document.getElementById("rs3").innerHTML = "Kiểm tra kích thước vừa nhập!"
+  }
+  var p =(num1+num2) * 2;
+  var s = num1 * num2;
+  document.getElementById("rs4").innerHTML= p;
+  document.getElementById("rs5").innerHTML=s ;
+}
+
+/**Bài 5: Tính tổng 2 ký số
+ * 
+ * input: Nhập số có 2 chữ số
+ * process: 
+ * 1: lấy input 
+ * 2: lấy hàng đơn vị = n % 2
+ * 3: lấy hàng chục = n / 10
+ * 4: tổng 2 ký số = hàng đơn vị + hàng chục 
+ * 4: in kết quả ra màn hình
+ * 
+ * output: tổng 2 ký số
+*/
+function ex5(){
+  var num = parseInt(document.getElementById("txtNumber").value);
+  var tens = Math.floor(num/10);
+  var units = num%10;
+  var rs = tens + units;
+  document.getElementById("rs6").innerHTML=rs ;
+  
+
+}
